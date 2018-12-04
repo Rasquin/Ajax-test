bb4db094a2ebd9a1214769b7af8986f740436cfa
Rasquin <yannymk@hotmail.com>
2018-12-04 11:51:32 +0000

    implement pagination

--- main1.js
+++ main1.js
@@ -30,9 +30,7 @@ function writeToDocument(type) {
 /*form of table*/
 
 
-const baseURL = "https://swapi.co/api/";
-
-function getData(type, cb) {
+function getData(url, cb) {
     var xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = function() {
@@ -41,7 +39,7 @@ function getData(type, cb) {
         }
     };
 
-    xhr.open("GET", baseURL + type + "/");
+    xhr.open("GET", url);
     xhr.send();
 }
 
@@ -49,35 +47,47 @@ function getTableHeaders(obj) {
     var tableHeaders = [];
 
     Object.keys(obj).forEach(function(key) {
-        tableHeaders.push(`<td>${key}</td>`); //here we designate what goes in each table cell
-    })
-    return `<tr>${tableHeaders}</tr>`
+        tableHeaders.push(`<td>${key}</td>`);
+    });
+
+    return `<tr>${tableHeaders}</tr>`;
 }
 
-function writeToDocument(type) {
-   
+function generatePaginationButtons(next, prev) {
+    if (next && prev) {
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>
+                <button onclick="writeToDocument('${next}')">Next</button>`;
+    } else if (next && !prev) {
+        return `<button onclick="writeToDocument('${next}')">Next</button>`;
+    } else if (!next && prev) {
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>`;
+    }
+}
+
+function writeToDocument(url) {
+    var tableRows = [];
     var el = document.getElementById("data");
-    el.innerHTML = ""; // Here we get the title of the column of data, and that is clean every time you click a new gruop (ex:planets)
 
-    getData(type, function(data) {
-        var tableRows = []; //now we want to add rows
+    getData(url, function(data) {
+        var pagination = "";
+
+        if (data.next || data.previous) {
+            pagination = generatePaginationButtons(data.next, data.previous);
+        }
         data = data.results;
-        var tableHeaders = getTableHeaders(data[0]); //thus we get each data and doesn matter the name of the column
+        var tableHeaders = getTableHeaders(data[0]);
 
-        data.forEach(function(item) { //here we write the content of each row
+        data.forEach(function(item) {
             var dataRow = [];
 
             Object.keys(item).forEach(function(key) {
                 var rowData = item[key].toString();
-                var truncatedData = rowData.substring(0, 15) //Here we truncate the data because it's too long, we take the 1st 15 characters
-                //dataRow.push(`<td>${item[key]}</td>`); //individual row
-            dataRow.push(`<td>${truncatedData}</td>`)//now we show is the truncated data
-                
+                var truncatedData = rowData.substring(0, 15);
+                dataRow.push(`<td>${truncatedData}</td>`);
             });
             tableRows.push(`<tr>${dataRow}</tr>`);
-
-            // el.innerHTML += "<p>" + item.name + "</p>"; // the += is for not overwriting, but that everything appear
         });
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`;
     });
 }
